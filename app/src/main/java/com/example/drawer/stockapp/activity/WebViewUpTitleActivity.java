6f6d2d71f23493163fa2d1204fdcbe393cdfddeb
marktype <850623696@qq.com>
2016-07-22 13:22:04 +0800

    07.22分享添加

--- app/src/main/java/com/example/drawer/stockapp/activity/WebViewUpTitleActivity.java
+++ app/src/main/java/com/example/drawer/stockapp/activity/WebViewUpTitleActivity.java
@@ -8,7 +8,10 @@ import android.widget.ImageView;
 
 import com.example.drawer.stockapp.R;
 
-public class WebViewUpTitleActivity extends BascActivity {
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+public class WebViewUpTitleActivity extends BascActivity implements View.OnClickListener{
     private String url = "https://www.baidu.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +39,60 @@ public class WebViewUpTitleActivity extends BascActivity {
     public void initWight(){
         ImageView mBackImg = (ImageView) findViewById(R.id.back_img);
 
-        mBackImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ImageView mShareImg = (ImageView) findViewById(R.id.web_image_three);
+
+
+        mBackImg.setOnClickListener(this);
+        mShareImg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.back_img:
                 finish();
-            }
-        });
+                break;
+            case R.id.web_image_three:
+                showShare();
+                break;
+        }
+    }
+
+    /**
+     * 分享
+     */
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("分享测试");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ShareSDK.stopSDK();
     }
 }
